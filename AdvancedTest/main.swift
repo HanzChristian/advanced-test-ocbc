9f7a9e1f14d1012ce00eb0286325788a580c58c4
HanzChristian <hanz.christian@ui.ac.id>
2023-08-02 10:54:45 +0700

    Refactor OOP

--- AdvancedTest/main.swift
+++ AdvancedTest/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+//Apply polymorphism through protocol
+protocol Vehicle{
+    func getMaximumSpeed()->Double
+}
+
+//Define Car that implements Vehicle protocol
+class Car: Vehicle{
+    //Set return type for car as 180
+    func getMaximumSpeed() -> Double {
+        return 180
+    }
+}
+
+//Define Bus that implements Vehicle protocol
+class Bus:Vehicle{
+    //Set return type for car as 160
+    func getMaximumSpeed() -> Double {
+        return 160
+    }
+}
+
+//Define Train that implements Vehicle protocol
+class Train:Vehicle{
+    //Set return type for car as 600
+    func getMaximumSpeed() -> Double {
+        return 600
+    }
+}
 
